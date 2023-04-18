927a6de1f5fb37d3ac9188023824c90508b1c6fc
Master Erik <davidmasterkire@gmail.com>
2023-04-17 21:29:15 -0600

    Agregamos productos a la seccion de tienda y creamos array de productos

---
+++ src/componentes/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return ( 
+        <div>
+            <h1>Blog</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga obcaecati eos quae nulla sapiente optio facere, quibusdam numquam autem aspernatur ut aut temporibus pariatur repudiandae aliquid, molestiae exercitationem magnam eum.</p>
+        </div>
+     );
+}
+ 
+export default Blog;
---
+++ src/componentes/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga obcaecati eos quae nulla sapiente optio facere, quibusdam numquam autem aspernatur ut aut temporibus pariatur repudiandae aliquid, molestiae exercitationem magnam eum.</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
